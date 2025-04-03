bb9a7718efdd42c416f531d2c0bf55282c5c63e6
SabrinaDiLeva <sabridileva@gmail.com>
2025-04-03 12:59:01 -0300

    import java util list

--- src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/model/History.java
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/model/History.java
@@ -1,5 +1,7 @@
 package com.proyecto.uade.GestionTurnoProfesionalSalud.model;
 
+import java.util.List;
+
 public class History {
     private Long id;
     private List<Appointment> appointments;
--- src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/model/MedicalExam.java
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/model/MedicalExam.java
@@ -1,5 +1,7 @@
 package com.proyecto.uade.GestionTurnoProfesionalSalud.model;
 
+import java.util.List;
+
 public class MedicalExam {
     private Long id;
     private List<String> images;
