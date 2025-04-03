0c4aa6539255d99d1cbf7a456f4c9dfb8d37fe42
SabrinaDiLeva <sabridileva@gmail.com>
2025-04-03 14:49:06 -0300

    service first version- WIP

---
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/dto/command/ProfessionalDTO.java
@@ -0,0 +1,7 @@
+package com.proyecto.uade.GestionTurnoProfesionalSalud.dto.command;
+
+import java.io.Serializable;
+
+public class ProfessionalDTO implements Serializable {
+
+}
--- src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/service/ProfessionalService.java
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/service/ProfessionalService.java
@@ -1,5 +1,50 @@
 package com.proyecto.uade.GestionTurnoProfesionalSalud.service;
 
-public class ProfessionalService {
-    
+import com.proyecto.uade.GestionTurnoProfesionalSalud.dto.command.ProfessionalDTO;
+import com.proyecto.uade.GestionTurnoProfesionalSalud.model.Professional;
+import com.proyecto.uade.GestionTurnoProfesionalSalud.repository.IProfessionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+public class ProfessionalService implements IService<Professional, ProfessionalDTO>{
+    private IProfessionalRepository iProfessionalRepository;
+
+    @Autowired
+    public ProfessionalService(IProfessionalRepository iProfessionalRepository){
+        this.iProfessionalRepository = iProfessionalRepository;
+    }
+    @Override
+    public List<Professional> list() {
+
+        return iProfessionalRepository.findAll();
+    }
+
+    @Override
+    public Professional save(ProfessionalDTO dto) {
+        Professional p = new Professional();
+        return iProfessionalRepository.save(p);
+    }
+
+
+    @Override
+    public Professional find(Long id) {
+
+        return iProfessionalRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.find(id);
+        iProfessionalRepository.deleteById(id);
+    }
+
+    @Override
+    public Professional update(Long id, ProfessionalDTO dto) {
+        Professional p = this.find(id);
+        return null;
+    }
+
 }
