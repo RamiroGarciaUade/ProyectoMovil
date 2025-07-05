9d47e88166680794a5a789dda203f0e07658179a
gpedrosad <gpedrosa@uade.edu.ar>
2025-07-05 14:28:05 -0300

    implementar update profile en user service

--- src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/service/UserService.java
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/service/UserService.java
@@ -1,9 +1,11 @@
 package com.proyecto.uade.GestionTurnoProfesionalSalud.service;
 
 import com.proyecto.uade.GestionTurnoProfesionalSalud.dto.command.UserDTO;
+import com.proyecto.uade.GestionTurnoProfesionalSalud.dto.command.UserProfileDTO;
 import com.proyecto.uade.GestionTurnoProfesionalSalud.model.User;
 import com.proyecto.uade.GestionTurnoProfesionalSalud.repository.IUserRepository;
 import com.proyecto.uade.GestionTurnoProfesionalSalud.config.JwtUtil;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,11 +19,13 @@ import java.util.Map;
 @Service
 public class UserService implements IService<User, UserDTO> {
 
+    @Autowired
     private final IUserRepository iUserRepository;
+    @Autowired
     private final BCryptPasswordEncoder passwordEncoder;
+    @Autowired
     private final JwtUtil jwtUtil;
 
-    @Autowired
     public UserService(IUserRepository iUserRepository,
                        BCryptPasswordEncoder passwordEncoder,
                        JwtUtil jwtUtil) {
@@ -38,20 +42,17 @@ public class UserService implements IService<User, UserDTO> {
     @Override
     public User save(UserDTO userDTO) {
         if (iUserRepository.existsByEmailIgnoreCase(userDTO.getEmail())) {
-            throw new IllegalArgumentException("El email ya está registrado");
+            throw new IllegalArgumentException("Email ya registrado");
         }
-
-        String encryptedPassword = passwordEncoder.encode(userDTO.getPassword());
-
+        String encrypted = passwordEncoder.encode(userDTO.getPassword());
         User user = new User(
                 userDTO.getId(),
                 userDTO.getFirstName(),
                 userDTO.getLastName(),
                 userDTO.getEmail(),
-                encryptedPassword,
+                encrypted,
                 userDTO.getPhoneNumber()
         );
-
         return iUserRepository.save(user);
     }
 
@@ -63,23 +64,32 @@ public class UserService implements IService<User, UserDTO> {
 
     @Override
     public void delete(Long id) {
-        this.find(id);
+        find(id);
         iUserRepository.deleteById(id);
     }
 
     @Override
     public User update(Long id, UserDTO dto) {
-        User user = this.find(id);
-        UserDTO updatedDto = dto.update(user);
+        User user = find(id);
+        dto.update(user);
         return iUserRepository.save(user);
     }
 
     public Map<String, String> login(String email, String password) {
         User user = iUserRepository.findByEmail(email)
                 .filter(u -> passwordEncoder.matches(password, u.getPassword()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Credenciales inválidas"));
-
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
         String token = jwtUtil.generateToken(user.getEmail());
         return Collections.singletonMap("token", token);
+    }
+   ///UserProfileDTO
+    @Transactional
+    public User updateProfileById(Long id, UserProfileDTO dto) {
+        User user = find(id);
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        String digits = dto.getPhoneNumber().replaceAll("\\D+", "");
+        user.setPhoneNumber(Integer.parseInt(digits));
+        return iUserRepository.save(user);
     }
 }
