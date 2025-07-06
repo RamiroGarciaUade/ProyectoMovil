39e9ef08574b80488040a3214f1f5aa3d498cd10
gpedrosad <gpedrosa@uade.edu.ar>
2025-07-06 13:27:19 -0300

    lista de appointment de usuario

--- src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/repository/IAppointmentRepository.java
+++ src/main/java/com/proyecto/uade/GestionTurnoProfesionalSalud/repository/IAppointmentRepository.java
@@ -2,16 +2,13 @@ package com.proyecto.uade.GestionTurnoProfesionalSalud.repository;
 
 import com.proyecto.uade.GestionTurnoProfesionalSalud.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface IAppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByProfessional_Specialty_Id(Long specialtyId);
-
     List<Appointment> findByProfessional_Id(Long professionalId);
 
-
+    // === añadido: todos los turnos de un usuario ===
+    List<Appointment> findByUserId(Long userId);
 }
